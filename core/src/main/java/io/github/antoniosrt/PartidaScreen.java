32fc2202f4088fc59576846a8127d45813272976
Gustavo <gustavo.pottdeoliveira@gmail.com>
2024-12-09 20:10:22 -0300

    quiz

--- core/src/main/java/io/github/antoniosrt/PartidaScreen.java
+++ core/src/main/java/io/github/antoniosrt/PartidaScreen.java
@@ -7,10 +7,12 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.Random;
+
 public class PartidaScreen implements Screen {
     private final Main game;
     private SpriteBatch batch;
-    private Texture image;
+    private Texture image, image_questao, correto, errado;
     private BitmapFont font;
     private Partida partida;
     private boolean cartasSelecionadas = false;
@@ -19,6 +21,13 @@ public class PartidaScreen implements Screen {
     private long tempoDeEspera;
     private int quiz=0;
     private Botao  botaoP1, botaoP2;
+
+    private Questao questao;
+    private Botao BotaoA, BotaoB, BotaoC, BotaoD;
+    private int[] vetorquest = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private int[] resp_corretas = {3, 0, 2, 1, 3, 3, 2, 3, 1};
+    private int n_quest, respcorreta;
+    private String TexturePath = ("QUIZ/");
 
     public PartidaScreen(Main game, int classe) {
         this.game = game;
@@ -32,6 +41,34 @@ public class PartidaScreen implements Screen {
         botaoP1 = new Botao("powerupbuttons/ouron.png",120,60);
         botaoP1.setButtonX(-250);
         botaoP1.setButtonY(-20);
+
+        Random random = new Random();
+        n_quest = random.nextInt(vetorquest.length);
+        TexturePath = TexturePath + n_quest + ".png";
+        respcorreta = resp_corretas[n_quest-1];
+
+        questao = new Questao(n_quest,respcorreta,TexturePath);
+        image_questao = new Texture(TexturePath);
+        correto = new Texture("correto.png");
+        errado = new Texture("errado.png");
+
+        BotaoA = new Botao("botoesquiz/4.png", 80, 80);
+        BotaoA.setButtonY(200);
+        BotaoA.setButtonX((Gdx.graphics.getWidth() - BotaoA.getButtonWidth()) / 2);
+
+        BotaoB = new Botao("botoesquiz/5.png", 80, 80);
+        BotaoB.setButtonY(200);
+        BotaoB.setButtonX(50);
+        BotaoB.setButtonX((Gdx.graphics.getWidth() - BotaoB.getButtonWidth()) / 2);
+
+        BotaoC = new Botao("botoesquiz/6.png", 80, 80);
+        BotaoC.setButtonY(100);
+        BotaoC.setButtonX((Gdx.graphics.getWidth() - BotaoC.getButtonWidth()) / 2);
+
+        BotaoD = new Botao("botoesquiz/7.png", 80, 80);
+        BotaoD.setButtonY(100);
+        BotaoD.setButtonX(50);
+        BotaoD.setButtonX((Gdx.graphics.getWidth() - BotaoD.getButtonWidth()) / 2);
 
         iniciarPartida();
     }
@@ -89,12 +126,24 @@ public class PartidaScreen implements Screen {
 
         if (powerup1){
             batch.draw(botaoP1.getButtonTexture(), botaoP1.getButtonX(), botaoP1.getButtonY(), botaoP1.getButtonWidth(), botaoP1.getButtonHeight());
+        } else {
+            quiz=0;
+            botaoP1.setButtonTexture("powerupbuttons/ouroff.png");
+            batch.draw(botaoP1.getButtonTexture(), botaoP1.getButtonX(), botaoP1.getButtonY(), botaoP1.getButtonWidth(), botaoP1.getButtonHeight());
         }
         if (powerup2){
             batch.draw(botaoP2.getButtonTexture(), botaoP2.getButtonX(), botaoP2.getButtonY(), botaoP2.getButtonWidth(), botaoP2.getButtonHeight());
+        } else {
+            botaoP1.setButtonTexture("powerupbuttons/theyoff.png");
+            batch.draw(botaoP2.getButtonTexture(), botaoP2.getButtonX(), botaoP2.getButtonY(), botaoP2.getButtonWidth(), botaoP2.getButtonHeight());
         }
+
         if (quiz == 1){
-                //tela questao nao seiii
+            batch.draw(image_questao, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            batch.draw(BotaoA.getButtonTexture(), BotaoA.getButtonX(), BotaoA.getButtonY(), BotaoA.getButtonWidth(), BotaoA.getButtonHeight());
+            batch.draw(BotaoB.getButtonTexture(), BotaoB.getButtonX(), BotaoB.getButtonY(), BotaoB.getButtonWidth(), BotaoB.getButtonHeight());
+            batch.draw(BotaoC.getButtonTexture(), BotaoC.getButtonX(), BotaoC.getButtonY(), BotaoC.getButtonWidth(), BotaoC.getButtonHeight());
+            batch.draw(BotaoD.getButtonTexture(), BotaoD.getButtonX(), BotaoD.getButtonY(), BotaoD.getButtonWidth(), BotaoD.getButtonHeight());
         }
 
         batch.end();
@@ -127,9 +176,48 @@ public class PartidaScreen implements Screen {
             }
         }
 
-        if (botaoP1.detectaClique() && powerup1) {
+        if (Gdx.input.justTouched() && powerup1) {
             quiz=1;
             botaoP1.setButtonTexture("powerupbuttons/ouroff.png");
+            if (BotaoA.detectaClique()) {
+                if (respcorreta==0){
+                    batch.draw(correto, 0, 0, 160, 80);
+                } else {
+                    batch.draw(errado, 0, 0, 160, 80);
+                    powerup1 = false;
+                }
+                dispose();
+            }
+
+            if (BotaoB.detectaClique()) {
+                if (respcorreta==1){
+                    batch.draw(correto, 0, 0, 160, 80);
+                } else {
+                    batch.draw(errado, 0, 0, 160, 80);
+                    powerup1 = false;
+                }
+                dispose();
+            }
+
+            if (BotaoC.detectaClique()) {
+                if (respcorreta==2){
+                    batch.draw(correto, 0, 0, 160, 80);
+                } else {
+                    batch.draw(errado, 0, 0, 160, 80);
+                    powerup1 = false;
+                }
+                dispose();
+            }
+
+            if (BotaoD.detectaClique()) {
+                if (respcorreta==3){
+                    batch.draw(correto, 0, 0, 160, 80);
+                } else {
+                    batch.draw(errado, 0, 0, 160, 80);
+                    powerup1 = false;
+                }
+                dispose();
+            }
         }
     }
         public static Boolean manualSleep(long millis) {
@@ -166,5 +254,12 @@ public class PartidaScreen implements Screen {
         batch.dispose();
         image.dispose();
         font.dispose();
+        image_questao.dispose();
+        BotaoA.dispose();
+        BotaoB.dispose();
+        BotaoC.dispose();
+        BotaoD.dispose();
+        botaoP1.dispose();
+        botaoP2.dispose();
     }
 }
